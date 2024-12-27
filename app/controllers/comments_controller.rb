e63cd766c9b87586da82003291441b062b9456ff
pooi <polischykpogi@gmail.com>
2024-12-27 06:04:31 +0300

    удаление комментариев

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,17 @@ class CommentsController < ApplicationController
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
-      end
+    end
+
+    def destroy
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
+      @comment.destroy
+      redirect_to article_path(@article), status: :see_other
+    end
     
-      private
-        def comment_params
-          params.require(:comment).permit(:commenter, :body, :status)
-        end
+    private
+      def comment_params
+        params.require(:comment).permit(:commenter, :body, :status)
+      end
 end
